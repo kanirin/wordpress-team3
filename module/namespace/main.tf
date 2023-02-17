737e9fb46476ea8691d6efc2ad4800c3cc825f08
kanirin <krinatova95@gmail.com>
2023-02-16 20:48:08 -0600

    deploed wordpress

---
+++ module/namespace/main.tf
@@ -0,0 +1,35 @@
+resource "kubernetes_namespace" "example" {
+  metadata {
+    annotations = var.annotations
+    labels      = var.labels
+    name        = var.name
+  }
+}
+ 
+
+
+# resource "kubernetes_resource_quota" "secrets" {
+#   metadata {
+#     name      = "secrets"
+#     namespace = kubernetes_namespace.example.metadata[0].name
+#   }
+#   spec {
+#     hard = {
+#       secrets = 100
+#     }
+#   }
+# }
+
+
+
+# resource "kubernetes_resource_quota" "configmaps" {
+#   metadata {
+#     name      = "configmaps"
+#     namespace = kubernetes_namespace.example.metadata[0].name
+#   }
+#   spec {
+#     hard = {
+#       configmaps = 100
+#     }
+#   }
+# }
