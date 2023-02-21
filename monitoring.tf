ff8ae94ff5986b5fc702c56d4afd4591a47f431a
DianaMac84 <107662802+DianaMac84@users.noreply.github.com>
2023-02-21 08:49:35 -0600

    Delete monitoring.tf

--- monitoring.tf
+++
@@ -1,6 +0,0 @@
-module "wordpress" {
-  source             = "../modules/monitoring"
-  google_domain_name = var.google_domain_name
-  PROJECT_ID         = var.PROJECT_ID
-  service_name       = "wordpress"
-}
