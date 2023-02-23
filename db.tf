90dcc4172fd7d4aa3e74af9dfb7d14703a6be892
kanirin <111390977+kanirin@users.noreply.github.com>
2023-02-22 23:57:11 -0600

    added sql

---
+++ db.tf
@@ -0,0 +1,8 @@
+module wordpress_db {
+    source          = "./module/cloudsql"
+    db_name         = 
+    dbinstance_name = 
+    db_username     =
+    db_password     =
+    db_host         =
+}
