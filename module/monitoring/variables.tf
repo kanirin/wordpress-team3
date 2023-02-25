6acf1c6f7a2569b420d7589912e0b76524c673ae
kanirin <111390977+kanirin@users.noreply.github.com>
2023-02-24 20:21:58 -0600

    Delete module directory

--- module/monitoring/variables.tf
+++
@@ -1,6 +0,0 @@
-variable "PROJECT_ID" {
-  type    = string
-  default = ""
-}
-variable "google_domain_name" {}
-variable "service_name" {}
--- module/namespace/output.tf
+++
@@ -1,6 +0,0 @@
-output "kubernetes_namespace" {
-  value = kubernetes_namespace.example.id
-}
-output "namespace" {
-  value = kubernetes_namespace.example.metadata[0].name
-}
